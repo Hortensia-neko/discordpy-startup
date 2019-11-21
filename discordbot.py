92ce68da0963a1197a3b52a1e8641c030a52f68b
Hortensia-neko <58004787+Hortensia-neko@users.noreply.github.com>
2019-11-21 15:27:06 +0900

    hufimin command

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -16,6 +17,13 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+
+@bot.command()
+async def hifumin(ctx):
+    numbers=["一","二","三","四","五","六","七","八","九"]
+    hihumin=["加藤"+numbers[random.randrange(len(numbers))]+numbers[random.randrange(len(numbers))]+numbers[random.randrange(len(numbers))]+numbers[random.randrange(len(numbers))]+"段"]
+    await ctx.send(hifumin)
 
 
 bot.run(token)
