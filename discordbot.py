330d87a6928eefb5a017cfbb21bc7d97f624f1dc
Hortensia-neko <58004787+Hortensia-neko@users.noreply.github.com>
2020-07-17 17:02:10 +0900

    hifuminがchoicesを使うように

--- discordbot.py
+++ discordbot.py
@@ -24,7 +24,7 @@ async def ping(ctx):
 @bot.command()
 async def hifumin(ctx):
     numbers=["一","二","三","四","五","六","七","八","九"]
-    hifumi=random.choice(numbers)+random.choice(numbers)+random.choice(numbers)+random.choice(numbers)
+    hifumi="".join(random.choices(numbers,k=4))
     if hifumi[3]==numbers[0]:
         hifumi=hifumi[:3]+"初"
     hifumin="加藤"+hifumi+"段"
